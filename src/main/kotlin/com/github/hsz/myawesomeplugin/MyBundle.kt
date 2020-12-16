985ba3a400e31bdf0faeed77ba9c836e20dc6ba7
GitHub Action <action@github.com>
2020-12-16 16:06:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/hsz/myawesomeplugin/MyBundle.kt
+++ src/main/kotlin/com/github/hsz/myawesomeplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hsz.myawesomeplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hsz/myawesomeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hsz/myawesomeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hsz.myawesomeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hsz.myawesomeplugin.MyBundle
 
 class MyApplicationService {
 
